a249eeaa5b097085a123aea4fe2a1e73d5cbc850
EC2 Default User <0429riku@gmail.com>
2022-01-22 05:20:25 +0000

    user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = current_user.books.all
+  end
+
+  def index
+    @book = Book.new
+    @users = User.all
+    @user = current_user
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to:"homes#top"
   get "about" => "homes#about"
   resources :books, only:[:create, :index, :show, :edit, :update, :destroy]
+  resources :users, only:[:show, :index, :edit, :update, :destroy]
   
 end
