6e13dc892059cd699c20b2d075b6385ac30a7984
EC2 Default User <0429riku@gmail.com>
2022-01-24 04:31:57 +0000

    layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
   root to:"homes#top"
+  get "about" => "homes#about" 
   
   resources :books, only:[:show, :index, :create, :edit, :update, :destroy]
   resources :users, only:[:show, :index, :edit, :update]
